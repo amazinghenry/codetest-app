08b77489ae4b64399eace4ffc66eb70b000d83c9
AmazingHenry <82852819+amazinghenry@users.noreply.github.com>
2024-11-27 12:45:31 +0000

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
       <Header />
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>links</div>
+    </div>
+  );
+};
+
+export default Navbar;
